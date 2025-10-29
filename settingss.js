25dcc65afb1a771770c1b89df3a89f2e795057d7
nate2101 <132824659+nate2101@users.noreply.github.com>
2025-10-29 19:22:17 +0300

    Update session ID, owner number, and prefix

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~5ZBniDrA#Abf_4HAZ-fkFmVb699ffGHwm9LETvaXknX--L9M8wgE",  // add your Session Id make sure it starts with blinder~
-  OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  SESSION_ID: "blinder~YNwh0SbK#m3BYoGB4Yl5ODUrq3Pn6Iw0cfnuG4ea7P6E2kYDaDQY",  // add your Session Id make sure it starts with blinder~
+  OWNER_NUMBER: "254721898295", // PUT OWNER NUMBER HERE
+  PREFIX: ",!." // prefix (e.g., ., /, !, *)
 };
