b835b2c3662e996c979e0b81dfd0ac92062bb2a5
nate2101 <132824659+nate2101@users.noreply.github.com>
2025-11-11 13:13:33 +0300

    Update SESSION_ID in settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~YNwh0SbK#m3BYoGB4Yl5ODUrq3Pn6Iw0cfnuG4ea7P6E2kYDaDQY",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~tNBzzSoY#R6_pzU6nDNTJwhmr3vEaH52HZ6-9uG-x6huaMIM_8rs",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254721898295", // PUT OWNER NUMBER HERE
   PREFIX: ",!." // prefix (e.g., ., /, !, *)
 };
