b218aa5e716ce8de2301a3c914161ff6255798fb
nate2101 <132824659+nate2101@users.noreply.github.com>
2025-11-25 04:06:13 +0300

    Change command prefix to a single dot

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_ID: "blinder~pNx3iCLI#n5QjnRqcO_pg6zgFdrPwGsByFwQcW-YEKQCW9ZLN3NA",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254721898295", // PUT OWNER NUMBER HERE
-  PREFIX: ".,!" // prefix (e.g., ., /, !, *)
+  PREFIX: "." // prefix (e.g., ., /, !, *)
 };
